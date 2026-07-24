60b68de7adb32510c079efeda3b42648601e9b4c
ceweaver11-cyber <ceweaver11@gmail.com>
2026-07-24 15:46:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,25 +18,61 @@ const anthropic = new Anthropic({
 const GITHUB_REPO = 'ceweaver11-cyber/IM-Article-Builder';
 const FILE_PATH = 'articles.json';
 
-// Master List of 500 Cities & Regions across Switzerland and France
+// Updated Master List of Key Cities & Regions across France and Switzerland
 const LOCATION_POOL = [
-  // Cantons & Key Regions (Switzerland)
-  "Zurich Canton", "Bern Canton", "Lucerne Canton", "Uri", "Schwyz", "Obwalden", "Nidwalden", "Glarus", "Zug Canton", "Fribourg Canton", "Solothurn Canton", "Basel-Stadt", "Basel-Landschaft", "Schaffhausen Canton", "Appenzell Ausserrhoden", "Appenzell Innerrhoden", "St. Gallen Canton", "Graubünden", "Aargau", "Thurgau", "Ticino", "Vaud", "Valais", "Neuchâtel Canton", "Geneva (Grand Genève)", "Jura Canton",
-  
-  // Swiss Cities & Municipalities
-  "Geneva", "Lausanne", "Montreux", "Vevey", "Nyon", "Morges", "Renens", "Pully", "Vernier", "Lancy", "Meyrin", "Carouge", "Onex", "Thônex", "Versoix", "Chêne-Bougeries", "Grand-Saconnex", "Yverdon-les-Bains", "Gland", "Rolle", "Aigle", "Payerne", "Lutry", "Echallens", "Villeneuve",
-  "Zurich", "Winterthur", "Uster", "Dübendorf", "Dietikon", "Wetzikon", "Kloten", "Horgen", "Wädenswil", "Bülach", "Opfikon", "Schlieren", "Adliswil", "Regensdorf", "Volketswil", "Thalwil", "Stäfa", "Illnau-Effretikon", "Küsnacht", "Meilen",
-  "Basel", "Bern", "Lucerne", "Zug", "Biel/Bienne", "Thun", "Köniz", "La Chaux-de-Fonds", "Fribourg", "Schaffhausen", "Chur", "Neuchâtel", "Solothurn", "Olten", "Aarau", "Baden", "Wettingen", "Riehen", "Allschwil", "Emmen", "Kriens", "Baar", "Cham", "Muttenz", "Liestal",
-  "St. Gallen", "Lugano", "Bellinzona", "Locarno", "Mendrisio", "Sion", "Sierre", "Martigny", "Monthey", "Wil", "Gossau", "Frauenfeld", "Arbon", "Kreuzlingen", "Rapperswil-Jona", "Herisau", "Davos", "St. Moritz", "Zermatt", "Brig-Glis",
+  // --- FRANCE ---
+  // Greater Paris (Île-de-France)
+  "Greater Paris (Île-de-France)", "Paris 1st–4th (Historic Center)", "Paris 6th (Saint-Germain)", "Paris 7th/8th (Eiffel/CBD)", "Paris 9th/10th (Tech/Gentrification)", "Paris 11th (Density/Rentals)", "Paris 15th/16th (Prime Residential)",
+  "Neuilly-sur-Seine", "Boulogne-Billancourt", "Levallois-Perret", "Issy-les-Moulineaux", "La Défense (Puteaux/Courbevoie)", "Versailles",
+  "Saint-Ouen", "Montreuil", "Pantin", "Vincennes", "Saint-Denis",
 
-  // Haute-Savoie, Pays de Gex, Savoie, Ain, Doubs & Jura
-  "Annecy", "Annemasse", "Thonon-les-Bains", "Évian-les-Bains", "Saint-Julien-en-Genevois", "Ferney-Voltaire", "Gex", "Divonne-les-Bains", "Cluses", "Sallanches", "Chamonix-Mont-Blanc", "Rumilly", "Bonneville", "Gaillard", "Passy", "Megève", "Saint-Gervais-les-Bains", "La Clusaz", "Le Grand-Bornand", "Morzine", "Les Gets", "Châtel", "Cran-Gevrier", "Seynod", "Meythet", "Ambilly", "Ville-la-Grand", "Vetraz-Monthoux", "Cranves-Sales", "Reignier-Ésery", "Douvaine", "Sciez", "Publier", "Neuvecelle", "Lugrin", "Saint-Cergues", "Archamps", "Collonges-sous-Salève", "Neydens", "Beaumont", "Viry", "Vulbens", "Valleiry", "Frangy", "Seyssel", "Thorens-Glières", "Groisy", "Saint-Jorioz", "Sévrier", "Talloires-Montmin", "Faverges-Seythenex", "Doussard", "Fillière", "Pers-Jussy", "Fillinges", "Viuz-en-Sallaz", "Taninges", "Samoëns", "Les Houches", "Servoz",
-  "Chambéry", "Aix-les-Bains", "Albertville", "Bourg-en-Bresse", "Oyonnax", "Bellegarde-sur-Valserine", "Belley", "Saint-Genis-Pouilly", "Thoiry", "Prévessin-Moëns", "Ornex", "Cessy", "Saint-Jean-de-Maurienne", "Bourg-Saint-Maurice", "Tignes", "Val-d'Isère", "Courchevel", "Méribel", "Les Allues", "Moûtiers", "La Léchère", "Ugine", "Challes-les-Eaux", "La Motte-Servolex", "Saint-Alban-Leysse", "Cognin", "Montmélian", "Saint-Pierre-d'Albigny", "Gilly-sur-Isère", "Yenne", "Lagnieu", "Ambérieu-en-Bugey", "Meximieux", "Montluel", "Miribel", "Trévoux", "Jassans-Riottier", "Châtillon-sur-Chalaronne", "Villars-les-Dombes", "Besançon", "Pontarlier", "Morteau", "Maîche", "Valdahon", "Baume-les-Dames", "Montbéliard", "Lons-le-Saunier", "Dole", "Saint-Claude", "Champagnole", "Morez", "Les Rousses", "Poligny", "Arbois", "Salins-les-Bains", "Saint-Amour", "Orgelet", "Clairvaux-les-Lacs", "Belfort", "Delle",
+  // French Riviera & Provence (PACA)
+  "Nice", "Cannes", "Saint-Tropez", "Antibes & Cap d'Antibes", "Saint-Jean-Cap-Ferrat", "Villefranche-sur-Mer",
+  "Marseille", "Aix-en-Provence", "Avignon", "Toulon",
 
-  // Lyon Area & Broader French Regions/Cities
-  "Lyon", "Villeurbanne", "Vénissieux", "Vaulx-en-Velin", "Saint-Priest", "Caluire-et-Cuire", "Bron", "Meyzieu", "Rillieux-la-Pape", "Decines-Charpieu", "Oullins-Pierre-Bénite", "Sainte-Foy-lès-Lyon", "Tassin-la-Demi-Lune", "Écully", "Saint-Genis-Laval", "Givors", "Villefranche-sur-Saône", "Saint-Étienne", "Roanne", "Saint-Chamond", "Firminy", "Grenoble", "Échirolles", "Saint-Martin-d'Hères", "Voiron", "Meylan", "Valence", "Romans-sur-Isère", "Montélimar", "Vienne", "Bourgoin-Jallieu", "L'Isle-d'Abeau", "Villefontaine", "Clermont-Ferrand", "Riom", "Cournon-d'Auvergne", "Vichy", "Moulins", "Montluçon", "Aurillac", "Le Puy-en-Velay", "Thiers", "Issoire", "Chamalières", "Annonay", "Aubenas", "Tournon-sur-Rhône", "Privas", "Bourg-lès-Valence", "Nyons", "Crest", "Die", "Tarare", "Belleville-en-Beaujolais", "Anse", "Craponne", "Francheville", "Mions", "Genas",
-  "Auvergne-Rhône-Alpes", "Bourgogne-Franche-Comté", "Bretagne", "Centre-Val de Loire", "Corse", "Grand Est", "Hauts-de-France", "Île-de-France", "Normandie", "Nouvelle-Aquitaine", "Occitanie", "Pays de la Loire", "Provence-Alpes-Côte d'Azur",
-  "Strasbourg", "Mulhouse", "Colmar", "Reims", "Metz", "Nancy", "Troyes", "Charleville-Mézières", "Thionville", "Épinal", "Dijon", "Nevers", "Auxerre", "Mâcon", "Chalon-sur-Saône", "Beaune", "Marseille", "Nice", "Toulon", "Aix-en-Provence", "Avignon", "Cannes", "Antibes", "Grasse", "Menton", "Toulouse", "Montpellier", "Nîmes", "Perpignan", "Béziers", "Narbonne", "Carcassonne", "Bordeaux", "Limoges", "Poitiers", "La Rochelle", "Pau", "Bayonne", "Biarritz", "Nantes", "Saint-Nazaire", "Angers", "Le Mans", "Laval", "La Roche-sur-Yon", "Rennes", "Brest", "Quimper", "Lorient", "Vannes", "Saint-Malo", "Rouen", "Le Havre", "Caen", "Lille", "Amiens", "Dunkerque", "Calais", "Valenciennes", "Arras", "Tours", "Orléans", "Bourges", "Blois", "Chartres", "Versailles", "Saint-Germain-en-Laye", "Fontainebleau", "Boulogne-Billancourt", "Neuilly-sur-Seine"
+  // French Alps (Mont-Blanc & Tarentaise)
+  "Courchevel", "Val d'Isère", "Méribel", "Megève", "Chamonix-Mont-Blanc", "Morzine", "Val Thorens",
+
+  // Auvergne-Rhône-Alpes & French-Geneva Border
+  "Lyon (Presqu'île, 6th Arr., Part-Dieu)", "Villeurbanne",
+  "Annecy (Lakefront)", "Grenoble", "Saint-Étienne",
+  "Annemasse", "Ferney-Voltaire", "Saint-Julien-en-Genevois", "Évian-les-Bains", "Divonne-les-Bains",
+
+  // Atlantic Coast & South West
+  "Bordeaux", "Mérignac",
+  "Biarritz", "Saint-Jean-de-Luz", "Bayonne", "Arcachon & Cap Ferret", "La Rochelle",
+  "Toulouse", "Montpellier",
+
+  // Grand Est, North & Western France
+  "Strasbourg", "Mulhouse", "Reims", "Colmar",
+  "Lille", "Nantes", "Rennes", "Angers", "Le Touquet",
+
+  // --- SWITZERLAND ---
+  // Lake Geneva Region (Arc Lémanique)
+  "Geneva City (Cité-Centre, Plainpalais, Eaux-Vives)", "Cologny", "Carouge", "Lancy (Pont-Rouge CBD)", "Meyrin",
+  "Lausanne (Ouchy, EPFL hub)", "Montreux", "Vevey", "Nyon", "Morges", "Lutry",
+
+  // Greater Zurich & Low-Tax Hubs
+  "Zurich City (Enge, Seefeld, Zurich West)", "Küsnacht (\"Gold Coast\")", "Zollikon", "Winterthur", "Oerlikon/Wallisellen",
+  "Zug City", "Baar",
+
+  // Central Switzerland
+  "Lucerne City", "Meggen", "Wollerau / Freienbach", "Weggis / Vitznau",
+
+  // Northwestern Switzerland (Basel & Pharma Hub)
+  "Basel City", "Riehen", "Allschwil", "Binningen",
+  "Baden", "Olten",
+
+  // Capital & Western Switzerland
+  "Bern City", "Thun", "Fribourg City", "Neuchâtel", "Biel/Bienne",
+
+  // Swiss Alpine & Resort Real Estate
+  "Zermatt", "Verbier", "Crans-Montana", "Sion", "Visp",
+  "St. Moritz (Engadin)", "Davos", "Klosters",
+
+  // Canton Ticino & St. Gallen
+  "Lugano", "Locarno", "Ascona",
+  "St. Gallen City"
 ];
 
 /**
